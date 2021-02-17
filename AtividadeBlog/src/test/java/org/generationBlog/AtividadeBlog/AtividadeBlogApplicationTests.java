3d178b66c624654bf7a5619dbf0350536ba9f5a6
ShenSouza <ds052798ts@gmail.com>
2021-02-17 18:47:05 -0300

    Projeto blog e atividade

---
+++ AtividadeBlog/src/test/java/org/generationBlog/AtividadeBlog/AtividadeBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generationBlog.AtividadeBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
