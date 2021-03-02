3a8034e64833932d56a39fbe97e097a119c55e82
ShenSouza <ds052798ts@gmail.com>
2021-03-02 11:20:24 -0300

    Implementando camadas de Security

---
+++ blogPessoal2.0/src/main/java/org/generate/blogPessoal20/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package org.generate.blogPessoal20.repository;
+
+import java.util.Optional;
+
+import org.generate.blogPessoal20.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+	public Optional<Usuario> findByUsuario(String usuario);
+}
